88a883c5f86aa7535888dcb289d08d334edae37a
Ty Loftin <tyloftin4@gmail.com>
2025-07-13 14:36:25 -0500

    Updates

---
+++ _app/immutable/chunks/Bmr1Qjey.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_apyln9?.base??"/mathdict",a=globalThis.__sveltekit_apyln9?.assets??s;export{a,s as b};
--- _app/immutable/chunks/DA2eBNHG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_11obkd8?.base??"/mathdict",t=globalThis.__sveltekit_11obkd8?.assets??s;export{t as a,s as b};
