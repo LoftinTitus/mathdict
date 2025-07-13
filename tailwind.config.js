91c1ec8a6ea1590f6d9702114c5b5a5d9d14ceef
Ty Loftin <tyloftin4@gmail.com>
2025-07-13 09:24:23 -0500

    fixing depends

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+  ],
+  plugins: [
+    skeleton({
+      themes: { preset: [ "modern" ] }
+    })
+  ],
+}
